457f77eef3389909b6a77fc81f3867576f41df2d
pxeger <_@pxeger.com>
2021-12-26 13:46:07 +1100

    go fmt

--- sigqueue.go
+++ sigqueue.go
@@ -5,30 +5,30 @@ package sigqueue
 // #include <signal.h>
 // char *get_errno() { return strerror(errno); }
 // int sigqueue_int(int pid, int signal, int value) {
-    // union sigval sigval;
-    // sigval.sival_int = value;
-    // return sigqueue(pid, signal, sigval);
+// union sigval sigval;
+// sigval.sival_int = value;
+// return sigqueue(pid, signal, sigval);
 // }
 import "C"
 
 type Error struct {
-    error
-    msg string
+	error
+	msg string
 }
 
 func (e *Error) Error() string {
-    return e.msg
+	return e.msg
 }
 
 func Sigqueue(pid, signal, value int) error {
-    ret := C.sigqueue_int(C.int(pid), C.int(signal), C.int(value))
-    switch ret {
-    case 0:
-        // success
-        return nil
-    case -1:
-        return &Error{ msg: C.GoString(C.get_errno()) }
-    default:
-        panic(ret) // unexpected return value from sigqueue
-    }
+	ret := C.sigqueue_int(C.int(pid), C.int(signal), C.int(value))
+	switch ret {
+	case 0:
+		// success
+		return nil
+	case -1:
+		return &Error{msg: C.GoString(C.get_errno())}
+	default:
+		panic(ret) // unexpected return value from sigqueue
+	}
 }
